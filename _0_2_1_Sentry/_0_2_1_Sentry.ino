b34591da0b6ecd910642b7e8119cd0bf0e4fb8d1
MarcoS <37008802+Infinity315@users.noreply.github.com>
2019-04-27 20:41:51 -0600

    Added features (debug info, and targetting software)
    Added information at startup to be displayed on LCD
    
    Added targeting routine and parsing inputs aswell as ability to tell Raspberry Pi when command is completed and is ready for another command.

--- _0_2_1_Sentry/_0_2_1_Sentry.ino
+++ _0_2_1_Sentry/_0_2_1_Sentry.ino
@@ -1,9 +1,9 @@
 // Created By Marco Sin
 // Purpose of program: Control Robot
-// Ver. 0.2.1 (As of April 20)
+// Ver. 0.3.1 (As of April 26)
 /*
 Program receives incoming float values over serial.  Then handles
-motor controls and uses a gyroscope to precisely aim and shoot at
+motor controls and uses a gyroscope to (hopefully!) precisely aim and shoot at
 stationary targets
 */
 
@@ -16,37 +16,68 @@ Bugfixes: Added a garbage value to be displayed when no targets are present. ver
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <LCD.h>
+#include <MPU6050_tockn.h>
+
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-float f;
-int timer = 10;
+MPU6050 mpu6050(Wire);
+
+// Variables
+float targetAngle;
+float parsedAngle;
+
+// Pin(s)
+const int Ready = 12;
+
 
 void setup() {
   Serial.begin(115200);
   lcd.begin(16,2);
-  lcd.setCursor(0,0);
-  while (timer-- != 0) { // 10s warmup to allow for adequate calibration of Gyroscope
-    lcd.clear();
-    printf("Countdown", 0, 0);
-    printf(String(timer), 0, 1);
-    delay(1000); // Wait 10s
-  }
-  
+  pinMode(Ready, OUTPUT);
+  Wire.begin();
+  mpu6050.begin();
+  digitalWrite(Ready, HIGH);
+
+  // Just notifying user when program is ready!
+  printf("Calibrating gyro", 0, 0);
+  printf("Wait for 10s", 0, 1);
+  mpu6050.calcGyroOffsets(true);
+  lcd.clear();
+  printf("Calibration done", 0, 0);
+  printf("Waiting 4 Serial", 0, 1);
 }
 
 void loop() {
+  digitalWrite(Ready, LOW); // Means that arduino is ready for next command
   if(Serial.available()) {
-    f = Serial.parseFloat();
+    digitalWrite(Ready, HIGH); // When this pin is high, it means that the arduino is currently executing a command
+    parsedAngle = Serial.parseFloat();  // Angle which the Pi has sent
     lcd.clear();
-    if (70 > f > 0) { // Debug value is 100.0
-      printf("Turn right:", 0, 0);
-      printf(f, 11, 0);
-    }
-    else if (f < 0) {  // For some reason Python program sends this as a default value when there is no target
+    if (70 > parsedAngle > 0) { // Debug value is 100.0
+      mpu6050.update();  // update rotation values
       printf("Turn left:", 0, 0);
-      printf(f, 11, 0);
+      lcd.print(parsedAngle);
+      targetAngle = parsedAngle + mpu6050.getAngleZ(); // Get Target angle by finding sum of parsedAngle and current angle
+      while (targetAngle > mpu6050.getAngleZ()) {
+        mpu6050.update();
+        printf("Remaining:", 0, 1);
+        lcd.print(targetAngle - mpu6050.getAngleZ());
+      }
     }
-    else if (f >= 70) {
+    if (parsedAngle < 0) { 
+      mpu6050.update();
+      printf("Turn right:", 0, 0);
+      lcd.print(-parsedAngle);
+      targetAngle = parsedAngle + mpu6050.getAngleZ();
+      Serial.println(targetAngle);
+      while (targetAngle < mpu6050.getAngleZ()) {
+        mpu6050.update();
+        printf("Remaining:", 0, 1);
+        lcd.print(targetAngle - mpu6050.getAngleZ());
+      }
+    }
+    if (parsedAngle >= 70 || parsedAngle == 0) { // Raspberry Pi will send a value of 100.0 to indicate no targets identified
+      lcd.clear();
       printf("Targets N/A", 0, 0);
     }
   }
